7e8a929d93b52bf20d6275ec45bf878e0c20be33
Millefeuille <millefeuille42@proton.me>
2026-06-29 16:49:45 +0200

    Add support for custom matrix.to domains (#1)
    Signed-off-by: Millefeuille <millefeuille42@proton.me>

--- src/UserSettingsPage.cpp
+++ src/UserSettingsPage.cpp
@@ -95,6 +95,8 @@ UserSettings::load(std::optional<QString> profile)
     sortByAlphabet_       = settings.value("user/sort_by_alphabet", false).toBool();
     readReceipts_         = settings.value("user/read_receipts", true).toBool();
     theme_                = settings.value("user/theme", defaultTheme_).toString();
+    matrixToDomains_ =
+      settings.value("user/matrix_to_domains", defaultMatrixToDomains_).toStringList();
 
     font_ = settings.value("user/font_family", "").toString();
 
@@ -678,6 +680,16 @@ UserSettings::setTheme(QString theme)
 }
 
 void
+UserSettings::setMatrixToDomains(QStringList domains)
+{
+    if (domains == matrixToDomains_)
+        return;
+    matrixToDomains_ = domains;
+    save();
+    emit matrixToDomainsChanged(domains);
+}
+
+void
 UserSettings::setUseStunServer(bool useStunServer)
 {
     if (useStunServer == useStunServer_)
@@ -967,6 +979,7 @@ UserSettings::save()
     settings.setValue("desktop_notifications", hasDesktopNotifications_);
     settings.setValue("alert_on_notification", hasAlertOnNotification_);
     settings.setValue("theme", theme());
+    settings.setValue("matrix_to_domains", matrixToDomains_);
     settings.setValue("font_family", font_);
     settings.setValue("emoji_font_family", emojiFont_);
     settings.setValue("ringtone", ringtone_);
@@ -1058,6 +1071,8 @@ UserSettingsModel::data(const QModelIndex &index, int role) const
         switch (index.row()) {
         case Theme:
             return tr("Theme");
+        case MatrixToDomains:
+            return tr("matrix.to domains");
         case ScaleFactor:
             return tr("Scale factor");
         case MessageHoverHighlight:
@@ -1215,6 +1230,8 @@ UserSettingsModel::data(const QModelIndex &index, int role) const
         switch (index.row()) {
         case Theme:
             return themes.indexOf(i->theme());
+        case MatrixToDomains:
+            return i->matrixToDomains();
         case ScaleFactor:
             return utils::scaleFactor();
         case MessageHoverHighlight:
@@ -1362,6 +1379,8 @@ UserSettingsModel::data(const QModelIndex &index, int role) const
     } else if (role == Description) {
         switch (index.row()) {
         case Theme:
+        case MatrixToDomains:
+            return tr("Choose domains to handle as matrix.to links");
         case Font:
         case EmojiFont:
             return {};
@@ -1575,6 +1594,8 @@ UserSettingsModel::data(const QModelIndex &index, int role) const
         case ShowImage:
         case SendMessageKey:
             return Options;
+        case MatrixToDomains:
+            return MultiLineText;
         case TimelineMaxWidth:
         case PrivacyScreenTimeout:
             return Integer;
@@ -1784,6 +1805,13 @@ UserSettingsModel::setData(const QModelIndex &index, const QVariant &value, int 
 
             if (idx >= 0 && idx < themes.size()) {
                 i->setTheme(themes[idx]);
+                return true;
+            } else
+                return false;
+        }
+        case MatrixToDomains: {
+            if (value.canConvert<QStringList>()) {
+                i->setMatrixToDomains(value.toStringList());
                 return true;
             } else
                 return false;
@@ -2289,6 +2317,9 @@ UserSettingsModel::UserSettingsModel(QObject *p)
     auto s = UserSettings::instance();
     connect(s.get(), &UserSettings::themeChanged, this, [this]() {
         emit dataChanged(index(Theme), index(Theme), {Value});
+    });
+    connect(s.get(), &UserSettings::matrixToDomainsChanged, this, [this]() {
+        emit dataChanged(index(MatrixToDomains), index(MatrixToDomains), {Value});
     });
     connect(s.get(), &UserSettings::mobileModeChanged, this, [this]() {
         emit dataChanged(index(MobileMode), index(MobileMode), {Value});
